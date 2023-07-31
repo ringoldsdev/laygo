a25baf856420eed161d398498e1f3353d71f7ef7
ringoldsdev <ringolds@lescinskis.com>
2023-07-31 20:49:37 +0300

    chore: created initial setup

---
+++ readme.md
@@ -0,0 +1,59 @@
+## Truba
+
+Type-safe pipelines for Typescript.
+
+Building pipelines shouldn't be difficult but existing packages didn't fit the bill.
+
+**Rxjs** was too heavy for my needs
+
+**Highland.js** was good, but it doesn't handle async (read stackoverflow and the issue list)
+
+**Scramjet** doesn't have any type safety built into their setup
+
+**ts-functional-pipe** is too basic and I don't like the way `pipe` and `compose` functions are implemented.
+
+Goal is to take what I think are the best parts of those packages and create something very lightweight that also performs well.
+
+### Example
+
+```ts
+
+const result = await truba()
+    .from(["Hello", "world"])
+    .map(val => val.toUpperCase())
+    .filter(val => ["HELLO"].includes(val))
+    .split("")
+    .chunk(2)
+    .result();
+```
+
+Result will be `["WO","RL","D"]`
+
+### How it works
+
+By itself the example above is not that impressive but `from()` accepts streams and generators as well.
+
+You can output a generator or await a result, but you can also pipe output into a stream and properly await until no data is coming through anymore.
+
+Moreover, you can compose more complicated pipelines using the `through` function. It's incredubly useful when you have to reuse the same chunk of code across the codebase, but you care only about changing the core.
+
+```ts
+const schema = z.object({
+    id: z.number(),
+    name: z.string()
+});
+
+// you can be more strict with types as well
+const pipeline = (p: Pipeline<unknown>) => truba()
+    .from(fs.createReadStream("./source.json"))
+    .map(JSON.parse)
+    .through(p)
+    .map(JSON.stringify)
+    .append("\n")
+    .pipe(fs.createWriteStream("./destination.json"));
+
+const result = await pipeline((truba) =>
+  truba.map(schema.parse).filter(({ id }) => id < 100)
+);
+```
+
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,135 @@
-console.log("Hello");
+type Result<T> = T | Promise<T>;
+type Unarray<T> = T extends Array<infer U> ? U : T;
+
+async function* sourceGenerator<T>(source: T[]) {
+  for (const item of source) {
+    yield item;
+  }
+}
+
+async function* flat<T>(source: AsyncGenerator<T>) {
+  for await (const item of source) {
+    if (!Array.isArray(item)) {
+      yield item;
+      continue;
+    }
+    for (const subItem of item) {
+      yield subItem;
+    }
+  }
+}
+
+async function* chunk<T>(source: AsyncGenerator<T>, size: number) {
+  let buffer: T[] = [];
+  for await (const item of source) {
+    buffer.push(item);
+    if (buffer.length === size) {
+      yield buffer;
+      buffer = [];
+    }
+  }
+  if (buffer.length) {
+    yield buffer;
+  }
+}
+
+async function* map<T, U>(
+  source: Generator<T> | AsyncGenerator<T>,
+  fn: (val: T) => Result<U>
+) {
+  for await (const item of source) {
+    yield fn(item);
+  }
+}
+
+async function* filter<T>(
+  source: Generator<T> | AsyncGenerator<T>,
+  fn: (val: T) => Result<boolean>
+) {
+  for await (const item of source) {
+    if (await fn(item)) {
+      yield item;
+    }
+  }
+}
+
+async function result<T>(source: AsyncGenerator<T>) {
+  const res: T[] = [];
+  for await (const item of source) {
+    res.push(item);
+  }
+  return res;
+}
+
+async function* take<T>(source: AsyncGenerator<T>, count: number) {
+  const res: T[] = [];
+  for await (const item of source) {
+    res.push(item);
+    if (res.length === count) {
+      break;
+    }
+  }
+  yield* res;
+}
+
+// async function* through<T, U>(
+//   source: AsyncGenerator<T>,
+//   fn: (source: AsyncGenerator<T>) => AsyncGenerator<U>
+// ) {
+//   for await (const item of fn(source)) {
+//     yield item;
+//   }
+// }
+
+type Pipeline<T> = {
+  map: <U>(fn: (val: T) => Result<U>) => Pipeline<U>;
+  filter: (fn: (val: T) => Result<boolean>) => Pipeline<T>;
+  take: (count: number) => Pipeline<T>;
+  chunk: (size: number) => Pipeline<T[]>;
+  flat: () => Pipeline<Unarray<T>>;
+  // through: <U>(fn: (source: Pipeline<T>) => U) => Pipeline<U>;
+  result: () => Result<T[]>;
+  toGenerator: () => AsyncGenerator<T>;
+  toStream: () => AsyncIterable<T>;
+};
+
+const pipelineBase = <T>(source: AsyncGenerator<T>): Pipeline<T> => {
+  return {
+    map: <U>(fn: (val: T) => Result<U>) => pipelineBase(map(source, fn)),
+    filter: (fn: (val: T) => Result<boolean>) =>
+      pipelineBase(filter(source, fn)),
+    chunk: (size: number) => pipelineBase(chunk(source, size)),
+    take: (count: number) => pipelineBase(take(source, count)),
+    flat: () => pipelineBase(flat(source)),
+    // through: <U>(pipeline: (source: Pipeline<T>) => Pipeline<U>) =>
+    //   pipelineBase(pipeline),
+    result: () => result(source),
+    toGenerator: () => source,
+    toStream: () => ({
+      [Symbol.asyncIterator]() {
+        return source;
+      }
+    })
+  };
+};
+
+const pipeline = () => ({
+  from: <T>(source: T[]) => {
+    const generator = sourceGenerator(source);
+    return pipelineBase(generator);
+  }
+});
+
+(async () => {
+  const res = await pipeline()
+    .from(["abc", "def", "ghi"])
+    .map((val) => val.toUpperCase())
+    .map((val) => val.split(""))
+    .take(2)
+    .flat()
+    .chunk(1)
+    .flat()
+    // .through(pipeline => pipeline.map(val => val + val))
+    .result();
+  console.log(res);
+})();
