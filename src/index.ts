ff72afcf14bc0aed6b840bb62ec3208a4cfbde13
ringoldsdev <ringolds@lescinskis.com>
2023-08-27 22:18:54 +0300

    chore: added more todo items

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,9 @@ export * from "./producers";
 // TODO: create a consumer type that gets created asynchronously, has a process and end function
 
 // TODO: create a transformer type that gets created asynchronously, has a process and end function
-// TODO: implement all possible functions as the scan function
 
-// TODO: implement finish function in the reduce function that runs after everything has been processed
 // TODO: implement multiple emit support in the reduce function
-
-// It's very tempting to try and make reduce work as a consumer, but I think it's better to keep it as a transformer
-// Better to create a new function that defines all consumers
-
-// TODO: implement a generic map function that can emit and end processing
 
 // TODO: implement a buffer function that accumulates data from upstream and emits it downstream
 // TODO: add another property to all functions - context. Context should be fully extendable and it should be type safe by default. Every time context changes get made, they should get reflected in downstream functions
+// TODO: implement parallel function that requires an array of items so that it can take all items and run them in parallel. All responses get returned as an array or as they finish. Settings can be changed as needed
